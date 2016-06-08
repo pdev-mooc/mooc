8f448ae4569a6dcdebc9c336d1451a32887dbabb
Intissar <Intissar@hp>
2016-06-08 17:28:10 +0200

    Entity Comment

---
+++ mooc-ejb/src/main/java/com/mooc/domain/Comment.java
@@ -0,0 +1,45 @@
+package com.mooc.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Table(name = "comment")
+@Entity
+public class Comment implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected Integer id;
+	private String text;
+	private Date dateTime;
+	
+//	@ManyToOne
+//	private Learner ;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public Date getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime(Date dateTime) {
+		this.dateTime = dateTime;
+	}
+	
+	
+}
